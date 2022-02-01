afc93c161397091dbc9e15ff17e79e80be3f1bdf
sant <santdev404@gmail.com>
2022-02-01 13:56:46 -0600

    go-go

--- readme.md
+++ readme.md
@@ -1,1 +1,21 @@
 # testing-laravel Tdd
+
+## Intall Tailwind
+
+nvm install v7.5.0
+
+npm install
+
+npm run dev
+
+npm install tailwindcss@0.7.4 --save-dev
+
+### on vscode terminal
+
+npx tailwind init tailwind.js
+
+npm install laravel-mix-tailwind
+
+### recompile & watch changes
+
+npm run watch
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,18 +9,16 @@
 
     </div>
 
-    <ul>
-    @forelse ($projects as $project)
-        <li>
-            
-            <a href="{{$project->path()}}">{{$project->title}}</a> 
-        </li>
+    <div class="flex">
+        @forelse ($projects as $project)
+        <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
+            <h3 class="font-normal text-xl py-4">{{$project->title}}</h3>
+            <div class="text-grey">{{ str_limit($project->description, 100)}}</div>
+        </div>
+        @empty
+            <li>No project yet</li>  s
+        @endforelse
+    </div>
 
-    @empty
-
-        <li>No project yet</li>    
-
-    @endforelse
-    </ul>
 
 @endsection
