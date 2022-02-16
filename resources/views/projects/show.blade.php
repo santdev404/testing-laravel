faadfd2f75986255e00d033da515fa172199ed78
sant <santdev404@gmail.com>
2022-02-16 10:23:54 -0600

    render activity with polymorphism

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -74,6 +74,8 @@
             </div>
             <div class="lg:w-1/4 px-3">
                 @include('projects.card')
+
+                @include('projects.activity.card')
             </div>
         </div>
     </main>
